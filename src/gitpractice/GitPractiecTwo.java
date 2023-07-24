549208821b0a0781f728ce0c678c00d9f5874fce
1iqbal <140277206+1iqbal@users.noreply.github.com>
2023-07-24 09:47:54 -0400

    Update GitPractiecTwo.java
    The 3th changes for this project

--- src/gitpractice/GitPractiecTwo.java
+++ src/gitpractice/GitPractiecTwo.java
@@ -4,6 +4,9 @@ public class GitPractiecTwo {
 	public static void main(String[] args) {
 		
 		System.out.println("This is for push practiec");
+		System.out.println("This is for push practiec one");
+		System.out.println("This is for push practiec two");
+		
 	}
 
 }

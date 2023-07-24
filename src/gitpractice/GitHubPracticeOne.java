a941c47e60c2e64f2a0574281b1327168aeab844
Ahad <aaiqbal@gmail.com>
2023-07-24 09:22:46 -0400

    First updated of my code

--- src/gitpractice/GitHubPracticeOne.java
+++ src/gitpractice/GitHubPracticeOne.java
@@ -3,10 +3,15 @@ package gitpractice;
 public class GitHubPracticeOne {
 public static void main(String[] args) {
 	
-	int a = 10;
-	int b = 15;
-	int c = a+b;
+//	int a = 10;
+//	int b = 15;
+//	int c = a+b;
 	
-	System.out.println(c);
+	System.out.println(testOne(10, 5));
+	
 }
+	public static int testOne(int a, int b) {
+		return a-b;
+	}
 }
+

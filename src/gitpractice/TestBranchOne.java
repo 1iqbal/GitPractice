797b93b763ab943497584a46f9683ea202893471
Ahad <aaiqbal@gmail.com>
2023-07-25 08:49:56 -0400

    It should be the last update

---
+++ src/gitpractice/TestBranchOne.java
@@ -0,0 +1,16 @@
+package gitpractice;
+
+public class TestBranchOne {
+	public static void main(String[] args) {
+		
+	
+System.out.println(testBranch(20, 5));
+
+	}
+
+	public static int  testBranch(int x,  int y) {
+		
+		return x/y;
+	}
+
+}
